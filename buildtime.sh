22b816112a1fbfbe1a09565340c529893e7fff2e
Oliver Schönrock <oliver@schonrocks.com>
2026-03-25 01:05:39 +0000

    4 way matrix of gcc and clang with their stdlibs and optional pch
    have to actually build {fmt} with those respective stdlibs
    using pch across compilers also requires care

--- buildtime.sh
+++ buildtime.sh
@@ -1,40 +1,93 @@
 #/usr/bin/env bash
 
-CXX=${CXX:-g++-14}
-CXXFLAGS=${CXXFLAGS:---std=c++23}
-
-if [[ "$1" == "--pch" ]]
-then
-    for prg in iostream format print fmtformat fmtformat; do
-	echo -n "building $prg.h.gch: ";
-	if [[ "$prg" == "fmt"* ]]
-	then
-	    CXXF="$CXXFLAGS -Ifmt/include -Lfmt/build";
-	else
-	    CXXF="$CXXFLAGS"
-	fi
-	(time -p $CXX $CXXF -x c++-header $prg.h) 2>&1 | egrep real | awk '{print $2}'
-    done
-else
-    rm -f *.gch
-fi
-
-mkdir -p build
-
-# for prg in puts printf iostream format print; do
-for prg in puts printf iostream format print fmtformat fmtprint; do
-    echo -n "$prg: ";
-    if [[ "$prg" == "fmt"* ]]
+function run() {
+    $CXX --version | grep -E "GCC|clang" | tr "\n" "s"
+    echo -n " with $STDLIB"
+    if [[ "$1" == "--pch" ]]
     then
-	CXXF="$CXXFLAGS -Ifmt/include -Lfmt/build";
-	LDFLAGS="-lfmt" # needs to go after the source
+	echo " with pre compiled headers"
     else
-	CXXF="$CXXFLAGS"
-	LDFLAGS="-lstdc++exp"
+	echo
     fi
     
-    (time -p $CXX $CXXF -o build/$prg $prg.cpp $LDFLAGS) 2>&1 | egrep real | awk '{print $2}' | tr "\n" "s"
-    echo -n " => "
-    strip build/$prg
-    ls -sh build/$prg | awk '{print $1}'
-done
+    CXXFLAGS="-std=c++23"
+    if [[ "$CXX" == "clang++" ]]
+    then
+	# ONLY FOR clang, gcc doesn't normally support this switch
+	CXXFLAGS="$CXXFLAGS -stdlib=$STDLIB"
+	EXT="pch"
+    else
+	EXT="gch"
+    fi
+    cd fmt
+    echo -n "compiling {fmt} with the above..."
+    cmake -GNinja -B build/$CXX -DCMAKE_CXX_FLAGS="'$CXXFLAGS'" -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_BUILD_TYPE=Release >&-
+    cmake --build build/$CXX >&-
+    echo done
+    cd ..
+    
+    rm -f build/*
+    if [[ "$1" == "--pch" ]]
+    then
+	for prg in iostream format print fmtformat fmtformat; do
+	    echo -n "building $prg.h.$EXT: ";
+	    if [[ "$prg" == "fmt"* ]]
+	    then
+		CXXF="$CXXFLAGS -Ifmt/include -Lfmt/build/$CXX";
+	    else
+		CXXF="$CXXFLAGS"
+	    fi
+	    (time -p $CXX $CXXF -x c++-header $prg.h) 2>&1 | grep -E real | awk '{print $2}'
+	    # echo $CXX $CXXF -x c++-header $prg.h
+	    # $CXX $CXXF -x c++-header $prg.h
+	done
+    else
+	rm -f *.$EXT
+    fi
+
+    for prg in puts printf iostream format print fmtformat fmtprint; do
+	echo -n "$prg: ";
+	if [[ "$prg" == "fmt"* ]]
+	then
+	    CXXF="$CXXFLAGS -Ifmt/include -Lfmt/build/$CXX";
+	    LDFLAGS="-lfmt" # needs to go after the source
+	else
+	    CXXF="$CXXFLAGS"
+	    LDFLAGS=""
+	fi
+	if [[ "$1" == "--pch" && "$CXX" == "clang++" && -e $prg.h.pch ]]
+	then
+	    # clang needs to be told to use the pch explicitly
+	    CXXF="$CXXF -include-pch $prg.h.pch"
+	fi
+	
+	(time -p $CXX $CXXF -o build/$prg $prg.cpp $LDFLAGS) 2>&1 | grep -E real | awk '{print $2}' | tr "\n" "s"
+	# echo $CXX $CXXF -o build/$prg $prg.cpp $LDFLAGS
+	# $CXX $CXXF -o build/$prg $prg.cpp $LDFLAGS
+	echo -n " => "
+	strip build/$prg
+	ls -sh build/$prg | awk '{print $1}'
+    done
+}
+
+CXX=g++
+STDLIB=libstdc++
+run 
+
+echo
+
+CXX=clang++
+STDLIB=libc++
+run
+
+echo
+
+CXX=g++
+STDLIB=libstdc++
+run --pch
+
+echo
+
+CXX=clang++
+STDLIB=libc++
+run --pch
